c1d40d9e78e488ad6129ae492fa904a9a28fc769
Allen <fengyijun@yingyinglicai.com>
2017-12-28 10:30:35 +0800

    fix bug

--- bin/chrys.js
+++ bin/chrys.js
@@ -7,4 +7,5 @@ const version = require('../package.json').version;
 program
     .version(version)
     .command('init', 'init a react template')
+    .command('upload','upload upyun')
     .parse(process.argv);
